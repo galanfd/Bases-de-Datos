e50baadc76c706d69e2415285f6d65f28027aaeb
galanfd <53878034+galanfd@users.noreply.github.com>
2020-10-26 20:07:24 -0300

    Update instalaciones_libres.sql

--- Entrega3/Test/instalaciones_libres.sql
+++ Entrega3/Test/instalaciones_libres.sql
@@ -38,7 +38,7 @@ BEGIN
             end loop;
             espacio := info_instalacion.capacidad_instalacion - contador;
             porcentaje := espacio/info_instalacion.capacidad_instalacion;
-            insert into resultado(id_instal, fecha1, espacio, porcentaje);                   
+            execute 'insert into resultado VALUES(id_instal, fecha1, espacio, porcentaje)';                   
         end loop;
         if fecha1 = fecha2 then
             exit;
