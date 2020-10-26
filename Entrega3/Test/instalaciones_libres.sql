48d389641a7222063344530677cc637d4f66d865
galanfd <53878034+galanfd@users.noreply.github.com>
2020-10-26 18:11:02 -0300

    Update instalaciones_libres.sql

--- Entrega3/Test/instalaciones_libres.sql
+++ Entrega3/Test/instalaciones_libres.sql
@@ -27,17 +27,17 @@ BEGIN
             id_instal := info_instalacion.id_instalacion;
             contador := 0;
             for info1 in execute 'SELECT *  FROM Permisos_Pedidos, Instalaciones, Puerto_Instalacion, Puertos, Permisos, Permiso_muelle WHERE Permisos_Pedidos.id_instalacion = Instalaciones.id_instalacion AND Instalaciones.id_instalacion = Puerto_Instalacion.id_instalacion AND Puerto_Instalacion.id_puerto = Puertos.id_puerto AND Permisos_Pedidos.id_permiso = Permisos.id_permiso AND Permiso_muelle.id_permiso = Permisos.id_permiso AND Puertos.id_puerto = $1' using seleccion_puerto loop
-                if info1.atraque = fecha1 and info1.Instalaciones.id_instalacion = id_instal then
+                if info1.fecha_atraque = fecha1 and info1.Instalaciones.id_instalacion = id_instal then
                     contador := contador + 1;
                 end if;
             end loop;
             for info2 in execute 'SELECT * FROM Permisos_Pedidos, Instalaciones, Puerto_Instalacion, Puertos, Permisos, Permiso_muelle WHERE Permisos_Pedidos.id_instalacion = Instalaciones.id_instalacion AND Instalaciones.id_instalacion = Puerto_Instalacion.id_instalacion AND Puerto_Instalacion.id_puerto = Puertos.id_puerto AND Permisos_Pedidos.id_permiso = Permisos.id_permiso AND Permiso_muelle.id_permiso = Permisos.id_permiso AND Puertos.id_puerto = $1' using seleccion_puerto loop
-                if info2.atraque <= fecha1 and info2.Instalaciones.id_instalacion = id_instal  and info2.salida >fecha1 then
+                if info2.fecha_atraque <= fecha1 and info2.Instalaciones.id_instalacion = id_instal  and info2.salida >fecha1 then
                     contador := contador + 1;
                 end if;
             end loop;
-            espacio := info_instalacion.capacidad - contador;
-            porcentaje := espacio/info_instalacion.capacidad;
+            espacio := info_instalacion.capacidad_instalacion - contador;
+            porcentaje := espacio/info_instalacion.capacidad_instalacion;
             execute 'INSERT INTO resultado(id_instal, fecha1, espacio, porcentaje)';
         end loop;
         if fecha1 = fecha2 then
