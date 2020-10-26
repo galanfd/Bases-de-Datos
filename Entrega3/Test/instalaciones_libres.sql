672b81c8093aeffad725e191ab3091f27e38bb82
galanfd <53878034+galanfd@users.noreply.github.com>
2020-10-26 18:07:13 -0300

    Update instalaciones_libres.sql

--- Entrega3/Test/instalaciones_libres.sql
+++ Entrega3/Test/instalaciones_libres.sql
@@ -26,12 +26,12 @@ BEGIN
         for info_instalacion in execute 'SELECT Instalaciones.id_instalacion, Instalaciones.capacidad_instalacion FROM Puertos, Puerto_Instalacion, Instalaciones WHERE Puertos.id_puerto = Puerto_Instalacion.id_puerto AND Puerto_Instalacion.id_instalacion = Instalaciones.id_instalacion AND Puertos.id_puerto = $1' using seleccion_puerto loop
             id_instal := info_instalacion.id_instalacion;
             contador := 0;
-            for info1 in execute query1 loop
+            for info1 in execute 'SELECT *  FROM Permisos_Pedidos, Instalaciones, Puerto_Instalacion, Puertos, Permisos, Permiso_muelle WHERE Permisos_Pedidos.id_instalacion = Instalaciones.id_instalacion AND Instalaciones.id_instalacion = Puerto_Instalacion.id_instalacion AND Puerto_Instalacion.id_puerto = Puertos.id_puerto AND Permisos_Pedidos.id_permiso = Permisos.id_permiso AND Permiso_muelle.id_permiso = Permisos.id_permiso AND Puertos.id_puerto = $1' using seleccion_puerto loop
                 if info1.atraque = fecha1 and info1.Instalaciones.id_instalacion = id_instal then
                     contador := contador + 1;
                 end if;
             end loop;
-            for info2 in execute query2 loop
+            for info2 in execute 'SELECT * FROM Permisos_Pedidos, Instalaciones, Puerto_Instalacion, Puertos, Permisos, Permiso_muelle WHERE Permisos_Pedidos.id_instalacion = Instalaciones.id_instalacion AND Instalaciones.id_instalacion = Puerto_Instalacion.id_instalacion AND Puerto_Instalacion.id_puerto = Puertos.id_puerto AND Permisos_Pedidos.id_permiso = Permisos.id_permiso AND Permiso_muelle.id_permiso = Permisos.id_permiso AND Puertos.id_puerto = $1' using seleccion_puerto loop
                 if info2.atraque <= fecha1 and info2.Instalaciones.id_instalacion = id_instal  and info2.salida >fecha1 then
                     contador := contador + 1;
                 end if;
