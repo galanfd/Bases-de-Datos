70d43ca32fa151abcb3aab64dae6c02f1f82c6d1
galanfd <53878034+galanfd@users.noreply.github.com>
2020-10-27 15:55:49 -0300

    Update capacidad_barco.sql

--- Entrega3/Test/capacidad_barco.sql
+++ Entrega3/Test/capacidad_barco.sql
@@ -25,38 +25,22 @@ BEGIN
     CREATE TABLE espacio_barco(id_instal INT, tiene_capacidad varchar, posible_fecha_entrada timestamp);
     
     if tipo = 'muelle' then
-	loop
-	    for info_instalacion in execute 'SELECT Instalaciones.id_instalacion, Instalaciones.capacidad_instalacion FROM Puertos, Puerto_Instalacion, Instalaciones WHERE Puertos.id_puerto = Puerto_Instalacion.id_puerto AND Puerto_Instalacion.id_instalacion = Instalaciones.id_instalacion AND Instalaciones.tipo_instalacion = $1 AND Puertos.id_puerto = $2' using tipo, seleccion_puerto loop
-                id_instal := info_instalacion.id_instalacion;
-	        contador := 0;
-	        for info1 in execute 'SELECT *  FROM Permisos_Pedidos, Instalaciones, Puerto_Instalacion, Puertos, Permisos, Permiso_muelle WHERE Permisos_Pedidos.id_instalacion = Instalaciones.id_instalacion AND Instalaciones.id_instalacion = Puerto_Instalacion.id_instalacion AND Puerto_Instalacion.id_puerto = Puertos.id_puerto AND Permisos_Pedidos.id_permiso = Permisos.id_permiso AND Permiso_muelle.id_permiso = Permisos.id_permiso AND Puertos.id_puerto = $1' using seleccion_puerto loop
-                    if info1.fecha_atraque = fecha1 and info1.id_instalacion = id_instal then
-                        contador := contador + 1;
-                    end if;
-		end loop;	
-	    espacio := info_instalacion.capacidad_instalacion - contador;
-	    if espacio > 0 then
-	        estado := 'True';
-	    end if;
-	    if espacio = 0 then
-		estado := 'False';
-	    end if;
-	    insert into espacio VALUES(id_instal, fecha1, estado);
-            end loop;
-            if fecha1 = fecha2 then
-                exit;
-            end if;
-            fecha1 := fecha1 + interval '1' day;
-        end loop;
-	entra := 'False';
-	date := NULL;
-	for instal in execute 'SELECT * FROM espacio' loop
-	    if instal.tiene_capacidad = 'True' then
-		entra := 'True';
-		date := instal.fecha;
-	    end if;
-	end loop;
-	insert into espacio_barco VALUES(id_instal, entra, date);
+	for info_instalacion in execute 'SELECT Instalaciones.id_instalacion, Instalaciones.capacidad_instalacion FROM Puertos, Puerto_Instalacion, Instalaciones WHERE Puertos.id_puerto = Puerto_Instalacion.id_puerto AND Puerto_Instalacion.id_instalacion = Instalaciones.id_instalacion AND Instalaciones.tipo_instalacion = $1 AND Puertos.id_puerto = $2' using tipo, seleccion_puerto loop
+            id_instal := info_instalacion.id_instalacion;
+	    contador := 0;
+	    for info1 in execute 'SELECT *  FROM Permisos_Pedidos, Instalaciones, Puerto_Instalacion, Puertos, Permisos, Permiso_muelle WHERE Permisos_Pedidos.id_instalacion = Instalaciones.id_instalacion AND Instalaciones.id_instalacion = Puerto_Instalacion.id_instalacion AND Puerto_Instalacion.id_puerto = Puertos.id_puerto AND Permisos_Pedidos.id_permiso = Permisos.id_permiso AND Permiso_muelle.id_permiso = Permisos.id_permiso AND Puertos.id_puerto = $1' using seleccion_puerto loop
+                if date_part('day', timestamp info2.fecha_atraque) = date_part('day', timestamp fecha1) and date_part('month', timestamp info2.fecha_atraque) = date_part('month', timestamp fecha1) and date_part('year', timestamp info2.fecha_atraque) = date_part('year', timestamp fecha1) and info1.id_instalacion = id_instal then
+                    contador := contador + 1;
+                end if;
+	    end loop;	
+	espacio := info_instalacion.capacidad_instalacion - contador;
+	if espacio > 0 then
+	    estado := 'True';
+	end if;
+	if espacio = 0 then
+	    estado := 'False';
+	end if;
+	insert into espacio_barco VALUES(id_instal, fecha1, estado);
     end if;
 
     if tipo = 'astillero' then
